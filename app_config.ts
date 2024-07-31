e8eea0f796f14a01458b1576a44d59b0c103d871
kromate <akpananthony33@gmail.com>
2024-07-31 12:04:49 +0100

    init

--- app_config.ts
+++ app_config.ts
@@ -6,7 +6,7 @@
 export default {
     keepalive: true,
     head: {
-        title: 'Goals - Achieve your goals faster with actionable todo',
+        title: 'Bank - Get Financial Insights from your Bank statements',
         htmlAttrs: { lang: 'en' },
         viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
         bodyAttrs: { class: 'overflow-x-hidden' },
@@ -19,38 +19,38 @@ export default {
             },
             {
                 name: 'title',
-                content: 'Goals - Achieve your goals faster with actionable todo'
+                content: 'Bank - Get Financial Insights from your Bank statements'
             },
             {
                 name: 'description',
-                content: 'Generates a list of actionable steps and to-do to achieve your goals'
+                content: 'Get Financial Insights from your Bank statements'
             },
             {
                 name: 'twitter:title',
-                content: 'Goals - Achieve your goals faster with actionable todo'
+                content: 'Bank - Get Financial Insights from your Bank statements'
             },
-            { name: 'twitter:image', content: 'https://Goals.taaskly.site/og2.png' },
+            { name: 'twitter:image', content: 'https://bank.taaskly.site/og2.png' },
             {
                 name: 'twitter:description',
-                content: 'Generates a list of actionable steps and to-do to achieve your goals.'
+                content: 'Get Financial Insights from your Bank statements.'
             },
             { name: 'twitter:card', content: 'summary_large_image' },
             { name: 'twitter:site', content: '@kromate_24' },
             { name: 'twitter:creator', content: '@kromate_24' },
             {
                 property: 'og:title',
-                content: 'Goals | Achieve your goals faster with actionable todo'
+                content: 'Bank | Get Financial Insights from your Bank statements'
             },
             { name: 'google-site-verification', content: 'tWttF6w3RHPlNPm5u7KSRgh4lgkRUZ2Bwl6QzECjY18' },
             { property: 'og:type', content: 'website' },
-            { property: 'og:url', content: 'https://goals.taaskly.site/' },
-            { property: 'og:image', content: 'https://goals.taaskly.site/og2.png' },
-            { property: 'og:image:secure_url', content: 'https://goals.taaskly.site/og2.png' },
+            { property: 'og:url', content: 'https://bank.taaskly.site/' },
+            { property: 'og:image', content: 'https://bank.taaskly.site/og2.png' },
+            { property: 'og:image:secure_url', content: 'https://bank.taaskly.site/og2.png' },
             { property: 'og:image:type', content: 'image/png' },
-            { property: 'og:site_name', content: 'Goals' },
+            { property: 'og:site_name', content: 'Bank' },
             {
                 property: 'og:description',
-                content: 'Generates a list of actionable steps and to-do to achieve your goals.'
+                content: 'Get Financial Insights from your Bank statements'
             },
 
             { name: 'format-detection', content: 'telephone=no' }
